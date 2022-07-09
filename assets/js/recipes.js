58851bf9c1a0389f74177a51d2d2cb7b64bef280
Ron Dyar <ron@fromex.net>
2022-07-09 15:14:32 -0700

    add take out options, links only if not takeout

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,7 +1,19 @@
 ---
 ---
 
-const main = ["toast", "chili", "pizza"];
+const desserts = [ {% for dessert in site.desserts %} {title: "{{dessert.title}}", url: "{{dessert.url}}"}, {% endfor %} ];
 
 const main_courses = [ {% for recipe in site.main-courses %} {title: "{{recipe.title}}", url: "{{recipe.url}}"}, {% endfor %} ];
-const desserts = [ {% for dessert in site.desserts %} {title: "{{dessert.title}}", url: "{{dessert.url}}"}, {% endfor %} ];
+const take_out = [
+    {title: 'Pizza - Take out', url: ''},
+    {title: 'BBQ - Take out', url: ''},
+    {title: 'Mystic Grill - Take out', url: ''},
+    {title: 'Burgers and Brats' , url: ''},
+    {title: 'Chicken and Waffles' , url: ''},
+
+]
+
+//combine main_courses and take_out arrays
+const recipes = main_courses.concat(take_out);
+
+
