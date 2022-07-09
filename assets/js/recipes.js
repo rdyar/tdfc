72e699edce0fe296d6c18323f3f3e517125409f1
Ron Dyar <ron@fromex.net>
2022-07-09 14:35:15 -0700

    working with desserts

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -4,3 +4,4 @@
 const main = ["toast", "chili", "pizza"];
 
 const main_courses = [ {% for recipe in site.main-courses %} {title: "{{recipe.title}}", url: "{{recipe.url}}"}, {% endfor %} ];
+const desserts = [ {% for dessert in site.desserts %} {title: "{{dessert.title}}", url: "{{dessert.url}}"}, {% endfor %} ];
