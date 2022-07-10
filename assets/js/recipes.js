bebdd84a0ce5471748a1d2be0edd8eb1afb8b6e9
Ron Dyar <ron@fromex.net>
2022-07-10 15:09:23 -0700

    move some soups to dinners, add some desserts

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,15 +1,19 @@
 ---
 ---
 
-const desserts = [ {% for dessert in site.desserts %} {title: "{{dessert.title}}", url: "{{dessert.url}}"}, {% endfor %} ];
+const desserts = [  {title: "Chocolate Chip Cookies", url: ""},
+{title: "Berries and Ice Cream", url: ""}, {% for dessert in site.desserts %} {title: "{{dessert.title}}", url: "{{dessert.url}}"}, {% endfor %} ];
+
 
 const main_courses = [ {% for recipe in site.main-courses %} {title: "{{recipe.title}}", url: "{{recipe.url}}"}, {% endfor %} ];
 const take_out = [
     {title: 'Pizza - Take out', url: ''},
     {title: 'BBQ - Take out', url: ''},
     {title: 'Mystic Grill - Take out', url: ''},
-    {title: 'Burgers and Brats' , url: ''},
+    {title: 'Burgers and/or Brats' , url: ''},
     {title: 'Chicken and Waffles' , url: ''},
+    {title: 'Steak/Tri-tip on the BBQ' , url: ''},
+    {title: 'Kabobs on the BBQ' , url: ''},
 
 ]
 
